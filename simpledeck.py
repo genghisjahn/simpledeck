2dd3dc6e8b253c91fd7d1cdc111ba16e77c4f9bc
Jon Wear <jon@dramafever.com>
2014-02-09 21:15:24 -0500

    Removed deck print out loops.

--- simpledeck.py
+++ simpledeck.py
@@ -48,9 +48,5 @@ for s in suits:
 
      	deck.cards.append(card)
 
-deck.Shuffle(10)     		
+deck.Shuffle(10)
 
-for c in deck.cards:
-	print(str.format('The card is the {} of {}',c.name, c.suit.name))
-
-print(str.format("Card Count:{}",len(deck.cards)))
